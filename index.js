3d3b0221d57021a1dec36836074fb8a70b1cfca7
Sheila Samita <samitasheila1@gmail.com>
2025-03-07 17:32:09 +0300

    Included arrow functions

---
+++ index.js
@@ -0,0 +1,8 @@
+// Function expression to divide 2000 by 100
+const divide = (a = 2000, b = 100) => a / b;
+
+// Arrow function to square a number
+const square = x => x * x;
+
+// Arrow function to add two numbers
+const add = (a, b) => a + b;
